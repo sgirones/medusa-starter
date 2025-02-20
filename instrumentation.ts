583cfff3ff733c5d9d3b2cc484906dce6a8be908
Salvador Gironès <salvadorgirones@gmail.com>
2025-02-20 11:09:51 +0100

    add instrumentation

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,24 +1,31 @@
 // Uncomment this file to enable instrumentation and observability using OpenTelemetry
 // Refer to the docs for installation instructions: https://docs.medusajs.com/learn/debugging-and-testing/instrumentation
 
-// import { registerOtel } from "@medusajs/medusa"
-// // If using an exporter other than Zipkin, require it here.
-// import { ZipkinExporter } from "@opentelemetry/exporter-zipkin"
+import { registerOtel } from '@medusajs/medusa';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 
-// // If using an exporter other than Zipkin, initialize it here.
-// const exporter = new ZipkinExporter({
-//   serviceName: 'my-medusa-project',
-// })
+export function register() {
+    // If using an exporter other than Zipkin, initialize it here.
+    if (!process.env.OTLP_EXPORTER_URL) {
+        console.error(
+            'OTLP_EXPORTER_URL is not set. Skipping instrumentation.'
+        );
+        return;
+    }
 
-// export function register() {
-//   registerOtel({
-//     serviceName: 'medusajs',
-//     // pass exporter
-//     exporter,
-//     instrument: {
-//       http: true,
-//       workflows: true,
-//       query: true
-//     },
-//   })
-// }
+    const exporter = new OTLPTraceExporter({
+        url: process.env.OTLP_EXPORTER_URL,
+    });
+
+    registerOtel({
+        serviceName: 'medusajs',
+        // pass exporter
+        exporter,
+        instrument: {
+            http: true,
+            workflows: true,
+            query: true,
+            db: true,
+        },
+    });
+}
