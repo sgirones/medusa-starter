a037872ec404c85df3aa226e6930055cf244a136
Salvador Gironès <salvadorgirones@gmail.com>
2025-02-26 16:00:25 +0100

    [api] Add a /version path

---
+++ src/api/version/route.ts
@@ -0,0 +1,11 @@
+import type { MedusaRequest, MedusaResponse } from '@medusajs/framework/http';
+
+const { GITHUB_COMMIT, GITHUB_BRANCH, GITHUB_REPOSITORY } = process.env;
+
+export async function GET(req: MedusaRequest, res: MedusaResponse) {
+    res.json({
+        github_commit: GITHUB_COMMIT,
+        github_branch: GITHUB_BRANCH,
+        github_repository: GITHUB_REPOSITORY,
+    });
+}
