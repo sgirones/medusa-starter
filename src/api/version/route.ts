959e4af2752b36db936efad81aa6ba91e298a686
Salvador Gironès <salvadorgirones@gmail.com>
2025-02-26 16:52:24 +0100

    try a key with a value

--- src/api/version/route.ts
+++ src/api/version/route.ts
@@ -4,6 +4,7 @@ const { GITHUB_COMMIT, GITHUB_BRANCH, GITHUB_REPOSITORY } = process.env;
 
 export async function GET(req: MedusaRequest, res: MedusaResponse) {
     res.json({
+        foo: 'bar',
         github_commit: GITHUB_COMMIT,
         github_branch: GITHUB_BRANCH,
         github_repository: GITHUB_REPOSITORY,
