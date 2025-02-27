5b6433bb299eb884d5cb4ac2ece858a40eac9f27
Salvador Gironès <salvadorgirones@gmail.com>
2025-02-27 10:39:44 +0100

    fix env var

--- src/api/version/route.ts
+++ src/api/version/route.ts
@@ -1,12 +1,12 @@
 import type { MedusaRequest, MedusaResponse } from '@medusajs/framework/http';
 
-const { GITHUB_COMMIT, GITHUB_BRANCH, GITHUB_REPOSITORY } = process.env;
+const { GITHUB_COMMIT, GITHUB_BRANCHES, GITHUB_REPOSITORY } = process.env;
 
 export async function GET(req: MedusaRequest, res: MedusaResponse) {
     res.json({
         foo: 'bar',
         github_commit: GITHUB_COMMIT,
-        github_branch: GITHUB_BRANCH,
+        github_branches: GITHUB_BRANCHES,
         github_repository: GITHUB_REPOSITORY,
     });
 }
