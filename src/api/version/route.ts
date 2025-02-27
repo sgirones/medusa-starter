ec5c9cd7544582ae878795f88ff12681392e0378
Salvador Gironès <salvadorgirones@gmail.com>
2025-02-27 11:40:52 +0100

    remove unused fields

--- src/api/version/route.ts
+++ src/api/version/route.ts
@@ -4,9 +4,7 @@ const { GITHUB_COMMIT, GITHUB_BRANCHES, GITHUB_REPOSITORY } = process.env;
 
 export async function GET(req: MedusaRequest, res: MedusaResponse) {
     res.json({
-        foo: 'bar',
         github_commit: GITHUB_COMMIT,
-        github_branches: GITHUB_BRANCHES,
         github_repository: GITHUB_REPOSITORY,
     });
 }
