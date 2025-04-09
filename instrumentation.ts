a6f45c7df97db313bed85d4988c4377fb09e1eab
Salvador Gironès <salvadorgirones@gmail.com>
2025-04-09 17:38:01 +0200

    revert to a working state

--- instrumentation.ts
+++ instrumentation.ts
@@ -5,39 +5,38 @@ import { registerOtel } from '@medusajs/medusa';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 
 export function register() {
-    throw new Error('OTLP_EXPORTER_URL is not set. Skipping instrumentation.');
     // If using an exporter other than Zipkin, initialize it here.
-    // if (!process.env.OTLP_EXPORTER_URL) {
-    //     console.error(
-    //         'OTLP_EXPORTER_URL is not set. Skipping instrumentation.'
-    //     );
-    //     return;
-    // }
+    if (!process.env.OTLP_EXPORTER_URL) {
+        console.error(
+            'OTLP_EXPORTER_URL is not set. Skipping instrumentation.'
+        );
+        return;
+    }
 
-    // const exporter = new OTLPTraceExporter({
-    //     url: process.env.OTLP_EXPORTER_URL,
-    //     concurrencyLimit: process.env.OTLP_EXPORTER_CONCURRENCY_LIMIT
-    //         ? parseInt(process.env.OTLP_EXPORTER_CONCURRENCY_LIMIT)
-    //         : 500,
-    // });
+    const exporter = new OTLPTraceExporter({
+        url: process.env.OTLP_EXPORTER_URL,
+        concurrencyLimit: process.env.OTLP_EXPORTER_CONCURRENCY_LIMIT
+            ? parseInt(process.env.OTLP_EXPORTER_CONCURRENCY_LIMIT)
+            : 500,
+    });
 
-    // const sdk = registerOtel({
-    //     serviceName: 'medusajs',
-    //     exporter,
-    //     instrument: {
-    //         http: true,
-    //         workflows: true,
-    //         query: true,
-    //         db: true,
-    //     },
-    // });
+    const sdk = registerOtel({
+        serviceName: 'medusajs',
+        exporter,
+        instrument: {
+            http: true,
+            workflows: true,
+            query: true,
+            db: true,
+        },
+    });
 
-    // process.on('SIGTERM', () => {
-    //     sdk.shutdown()
-    //         .then(
-    //             () => console.log('OTEL SDK was shut down successfully'),
-    //             (err) => console.log('Error shutting down OTEL SDK', err)
-    //         )
-    //         .finally(() => process.exit(0));
-    // });
+    process.on('SIGTERM', () => {
+        sdk.shutdown()
+            .then(
+                () => console.log('OTEL SDK was shut down successfully'),
+                (err) => console.log('Error shutting down OTEL SDK', err)
+            )
+            .finally(() => process.exit(0));
+    });
 }
